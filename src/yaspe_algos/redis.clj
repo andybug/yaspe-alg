e528d902ea525d4376fa4fb5d69dc8660355f344
Andrew Fields <andybug10@gmail.com>
2016-05-28 14:10:34 -0500

    Implement get-game

--- src/yaspe_algos/redis.clj
+++ src/yaspe_algos/redis.clj
@@ -2,9 +2,14 @@
   (:require [taoensso.carmine :as car :refer (wcar)])
   (:gen-class))
 
+(use 'clojure.walk)
+
 ; setup connection to redis server
 (def redis-server nil)
 (defmacro wcar* [& body] `(car/wcar redis-server ~@body))
 
 (defn ping []
   (println (wcar* (car/ping))))
+
+(defn get-game [key]
+  (keywordize-keys (apply hash-map (wcar* (car/hgetall key)))))
